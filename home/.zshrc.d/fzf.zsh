c23910b0d015dc902d827c12d6604484a256fa4c
Daniel Rampelt <drampelt@gmail.com>
2021-11-20 12:30:19 -0500

    chore: some fzf and nvm things

--- home/.zshrc.d/fzf.zsh
+++ home/.zshrc.d/fzf.zsh
@@ -1,5 +1,19 @@
-export FZF_DEFAULT_COMMAND="fd . $HOME"
+export FZF_DEFAULT_COMMAND="fd ."
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-export FZF_ALT_C_COMMAND="fd -t d . $HOME"
+export FZF_ALT_C_COMMAND="fd -t d ."
+
+# export FZF_DEFAULT_COMMAND='git ls-tree -r --name-only HEAD 2>/dev/null || fd' # --type -f if you want only files
+# export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+# From https://github.com/silvanocerza/dotfiles/blob/d3cf4ed770f18ba81a9059f7dd93dc3a97381239/zsh/zshrc#L44-L55
+# If current selection is a text file shows its content,
+# if it's a directory shows its content, the rest is ignored
+export FZF_CTRL_T_OPTS="--preview-window wrap --preview '
+if [[ -f {} ]]; then
+    file --mime {} | grep -q \"text\/.*;\" && bat --color \"always\" {} || (tput setaf 1; file --mime {})
+elif [[ -d {} ]]; then
+    exa -l --color always {}
+else;
+    tput setaf 1; echo YOU ARE NOT SUPPOSED TO SEE THIS!
+fi'"
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
--- home/.zshrc.d/nvm.zsh
+++ home/.zshrc.d/nvm.zsh
@@ -8,6 +8,23 @@ if [ -s "$HOME/.nvm/nvm.sh" ] && [ ! "$(whence -w __init_nvm)" = function ]; the
   export NVM_DIR="$HOME/.nvm"
   [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
   declare -a __node_commands=('nvm' 'node' 'npm' 'yarn' 'gulp' 'grunt' 'webpack' 'coffee' 'gr')
+  load-nvmrc() {
+    local node_version="$(nvm version)"
+    local nvmrc_path="$(nvm_find_nvmrc)"
+
+    if [ -n "$nvmrc_path" ]; then
+      local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
+
+      if [ "$nvmrc_node_version" = "N/A" ]; then
+        nvm install
+      elif [ "$nvmrc_node_version" != "$node_version" ]; then
+        nvm use
+      fi
+    elif [ "$node_version" != "$(nvm version default)" ]; then
+      echo "Reverting to nvm default version"
+      nvm use default
+    fi
+  }
   function __init_nvm() {
     echo "Initializing NVM"
     for i in "${__node_commands[@]}"; do unalias $i; done
@@ -15,6 +32,11 @@ if [ -s "$HOME/.nvm/nvm.sh" ] && [ ! "$(whence -w __init_nvm)" = function ]; the
     unset __node_commands
     unset -f __init_nvm
     source <(npm completion) # NPM completions
+
+    autoload -U add-zsh-hook
+    add-zsh-hook chpwd load-nvmrc
+    load-nvmrc
+
     echo "NVM initialized"
   }
   for i in "${__node_commands[@]}"; do alias $i='__init_nvm && '$i; done
